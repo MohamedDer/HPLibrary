e073437ec25172f20ee29024c09649d6a936ff4b
Mohamed <mde@frianbiz.com>
2020-03-28 17:07:25 +0100

    Setup BookTableViewCell

--- Hawry Powter Library/Reusable Views/BookTableViewCell/BookTableViewCell.swift
+++ Hawry Powter Library/Reusable Views/BookTableViewCell/BookTableViewCell.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import Kingfisher
 
 class BookTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var book: Book?
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+//        setupCellLayouts()
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupCellLayouts()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
+    
+    func setupCellLayouts() {
+        containerView.layer.cornerRadius = 5
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor.darkGray.cgColor
+    }
+    
+    func setupCellWith(book: Book) {
+        self.book = book
+        synopsisLabel.text = book.synopsis?.first
+        titleLabel.text = book.title
+        if let urlString = book.coverURLString {
+            setupImageWith(URLString: urlString)
+        }
+    }
+    
+    func setupImageWith(URLString: String)  {
+        let url = URL(string: URLString)
+        if let url = url {
+            coverImageView.kf.setImage(with: url)
+        }
+    }
+    
     
 }
